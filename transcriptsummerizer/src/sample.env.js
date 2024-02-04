42b79460f1e4a359063bb45f0fa8f2ea625e6fe0
Charvi Shukla <67646308+charvishukla@users.noreply.github.com>
2024-02-04 15:57:26 -0800

    Add activity 1

---
+++ transcriptsummerizer/src/sample.env.js
@@ -0,0 +1,10 @@
+/**
+ * This is a sample enviornment file, make sure to follow the following
+ * instructions for it to work with your program
+ * Rename this file to env.js
+ * Add your API KEY
+ */
+
+export const env_var = {
+    "GEMINI_API_KEY" : "YOUR API KEY"
+};
